ca370dc2c8f4adee281ede92dfca49febbbbcfe4
zhmhbest <zhmhbest@gmail.com>
2019-12-16 20:24:26 +0800

    可视化管理

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from main.models import Student
-admin.site.register(Student)
-
 from main.models import Computer
+
+admin.site.register(Student)
 admin.site.register(Computer)
